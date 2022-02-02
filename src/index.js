cd57d34b6135ae8094b5c99e1aa6187d23366177
MYuPopov <beemax@bk.ru>
2022-02-02 18:59:41 +0300

    Added layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import slider from './modules/slider';
+import header from './modules/header';
 
 slider();
+header();
---
+++ src/modules/header.js
@@ -0,0 +1,5 @@
+const header = () => {
+	console.log('slider is loaded');
+};
+
+export default header;
