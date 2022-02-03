ded8d21d5ac760c886ba937b214a01033aa7350a
MYuPopov <beemax@bk.ru>
2022-02-04 00:01:06 +0300

    Trying to add Swiper

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,4 +1,27 @@
+import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
+
+Swiper.use([Navigation, Pagination]);
+// Swiper.use([Pagination]);
+// Swiper.use([Autoplay]);
+
 const slider = () => {
+	const swiper = new Swiper('.swiper', {
+		// Настройки слайдера
+		loop: true,
+		autoplay: {
+			delay: 10000,
+			disableOnInteraction: false,
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+	});
+
 	console.log('slider is loaded');
 };
 
